2cbfefa82c9cdfe02eb58bd7578374cc0ad202ef
Enric Cervera <ecervera@uji.es>
2023-06-06 14:57:48 +0200

    Use OpenCV4

--- src/tracking_node.cpp
+++ src/tracking_node.cpp
@@ -1,7 +1,7 @@
 #include <math.h>
 
 #include <nav_msgs/Odometry.h>
-#include <opencv/cv.h>
+#include "opencv2/opencv.hpp"
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
